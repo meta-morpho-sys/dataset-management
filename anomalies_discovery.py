748a0878823a6c04f3e3abb3acff48bd2d58c05d
Yuliya Nedyalkova <yuliya.nedyalkova@globallogic.com>
2024-03-22 11:08:43 +0000

    Rename

--- anomalies_discovery.py
+++ anomalies_discovery.py
@@ -7,11 +7,13 @@ def find_anomalies(df):
     duplicates_or_empty_unit_no = filtered_rows.duplicated(subset=['Name'], keep=False) | (filtered_rows['Unit No.'] == '')
     return filtered_rows[duplicates_or_empty_unit_no]
 
+
 def output_anomalies(df, path_to_output_file):
     df.to_csv(path_to_output_file, index=False)
 
+
 def visualise_anomalies(path_to_csv, output_directory):
-    path_to_output_file = f"{output_directory}/names_no_numbers.csv"
+    path_to_output_file = f"{output_directory}/names_with_blank_numbers.csv"
     df = load_and_clean_data(path_to_csv)
     anomalies = find_anomalies(df)
     output_anomalies(anomalies, path_to_output_file)
